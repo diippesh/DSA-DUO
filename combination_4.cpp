2faa39922e836841775a63c3a0566603ee1eed67
diippesh <dipeshbhatiya317@gmail.com>
2026-02-18 11:29:03 +0530

    5th qn

--- combination_4.cpp
+++ combination_4.cpp
@@ -3,12 +3,12 @@ public:
     int combinationSum4(vector<int>& nums, int target) {
         vector<unsigned int>dp(target+1,0);
         dp[0]=1;
-        for(int t =1;t<= target; t++){
-            for(auto num:nums){
+        for(int t =1;t<= target; t++)
+            for(auto num:nums)
                 if(t-num>=0)
                     dp[t]+=dp[t-num];
-            }
-        }
+            
+        
         return dp[target];
     }
 };
